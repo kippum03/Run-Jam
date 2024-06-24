a8e4a95edbe21feda9b4289a62724a0e7a74e9cf
kippum03 <kippum03@naver.com>
2024-06-23 22:16:35 -0400

    Spotipy app authorized

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify, send_from_directory
-from spotify_auth import get_user_playlists
-from poke_api import get_pokemon_data
+from flask import Flask, request, redirect, session, jsonify, url_for, send_from_directory
+from spotify_auth import get_user_playlists, get_auth_url, get_token
+import os
 
 app = Flask(__name__, static_folder='../frontend', static_url_path='/')
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 def home():
@@ -10,13 +11,22 @@ def home():
 
 @app.route('/playlists', methods=['GET'])
 def playlists():
-    playlists = get_user_playlists()
-    return jsonify(playlists)
+    if 'token_info' not in session:
+        return redirect(url_for('login'))
+    token_info = session['token_info']
+    return jsonify(get_user_playlists())
 
-@app.route('/pokemon/<name>', methods=['GET'])
-def pokemon(name):
-    data = get_pokemon_data(name)
-    return jsonify(data)
+@app.route('/login')
+def login():
+    auth_url = get_auth_url()
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    token_info = get_token(code)
+    session['token_info'] = token_info
+    return redirect(url_for('playlists'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- backend/spotify_auth.py
+++ backend/spotify_auth.py
@@ -1,13 +1,26 @@
+from dotenv import load_dotenv
+import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id='06d558d41a064279848b9881cd6b7686',
-    client_secret='Y80a4366ccb804f5b9e9f4b1c551b295b',
-    redirect_uri='http://localhost:5000/callback',
+load_dotenv()
+
+sp_oauth = SpotifyOAuth(
+    client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+    client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
+    redirect_uri=os.getenv('SPOTIPY_REDIRECT_URI'),
     scope='user-library-read playlist-modify-public'
-))
+)
+
+sp = spotipy.Spotify(auth_manager=sp_oauth)
 
 def get_user_playlists():
     results = sp.current_user_playlists()
     return results
+
+def get_auth_url():
+    return sp_oauth.get_authorize_url()
+
+def get_token(code):
+    token_info = sp_oauth.get_access_token(code)
+    return token_info
